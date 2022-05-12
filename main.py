c38ea1dd24e24dd6ed177b209552ca1f437be346
David Bohn <bohn.david.j@gmail.com>
2022-05-11 22:24:43 -0500

    Added features to print 2D grid. Refactored Player Class to GameBoard Class. Added functions to add Ship objects to Gameboard objects.

--- main.py
+++ main.py
@@ -7,111 +7,96 @@ ships = {"destroyer": 2,
          "carrier": 5,
          }
 
-possible_columns = ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10')
-possible_rows = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J')
+COLUMNS = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+ROWS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
 
 
-class Player(object):
+class GameBoard(object):
 
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        self.board = []
+        self.create_board()
+        self.guesses = []
         self.ships = []
-        self.hits = []
-        self.misses = []
+    
+    def create_board(self):
+        for row in range(len(ROWS)):
+            self.board.append([])
+            for col in range(len(COLUMNS)):
+                self.board[row].append(' ')
 
-    def add_ship(self, ship):
-        """
-        "This method adds a ship object to the player object ships list"
-        :param ship: ship to be added
-        :return: void
-        """
-        self.ships.append(ship)
+    def add_ships(self):
+        """Get a random position.
+        Check to see is random position on self.board == 'X'
+        Then instantiate a ship object and set self.board to of random postion to 'X'"""
+        for name, hp in ships.items():
+            rand_x, rand_y = random.choice(ROWS), random.choice(COLUMNS)
+            while self.board[GameBoard.covert_rows_to_nums(rand_x)][int(rand_y)-1] == "X":
+                rand_x, rand_y = random.choice(ROWS), random.choice(COLUMNS)
+            self.ships.append(Ship(name, hp, [rand_x, rand_y]))
+            self.board[GameBoard.covert_rows_to_nums(rand_x)][int(rand_y)-1] = "X"
 
+    def covert_rows_to_nums(row) -> int:
+        rows_to_nums = {"A": 0,"B": 1,"C": 2,"D": 3,"E": 4,"F": 5,"G": 6,"H": 7,"I": 8}
+        return rows_to_nums.get(row)
+        
+    
+    def print_board(self):
+        print('  1 2 3 4 5 6 7 8 9')
+        print('  -----------------')
+        row_number = 0
+        for row in self.board:
+            row_letter = ROWS[row_number]
+            print('{}|{}|'.format(row_letter, "|".join(row)))
+            row_number +=1
+        
 
 class Ship(object):
-
-    def __init__(self, name, hit_points=3):
+    def __init__(self, name: str, hit_points: int, positions: list):
         self.name = name
         self.hit_points = hit_points
-        self.alive = True
-        self.positions = []
-
-    def add_random_positions(self):
-        row = random.choice(possible_rows)
-        column = random.choice(possible_columns)
-        #need to add condition for if positions already are taken
-        self.positions.append([row.upper(), column])
-
-    def add_positions(self):
-        print(f"Please set a position for your {self.name}")
-        ship_pos = get_position()
-        #need to add condition for if positions already are taken
-        self.positions.append(ship_pos)
+        self.positions = positions
 
     def valid_directions(self, position: list, hit_points):
         directions = ["north", "south", "east", "west"]
         for val in position:
-            if val in (possible_rows[0:(hit_points-1)]):
+            if val in (ROWS[0:(hit_points-1)]):
                 directions.remove("north")
-            elif val in possible_rows[(-hit_points)+1::]:
+            elif val in ROWS[(-hit_points)+1::]:
                 directions.remove("south")
-            elif val in (possible_columns[0:(hit_points-1)]):
+            elif val in (COLUMNS[0:(hit_points-1)]):
                 directions.remove("west")
-            elif val in possible_columns[(-hit_points)+1::]:
+            elif val in COLUMNS[(-hit_points)+1::]:
                 directions.remove("east")
 
             return directions
 
 
-def get_position() -> list:
-    row = "-"
-    column = "0"
+    def get_position() -> list:
+        row = "-"
+        column = "0"
 
-    while row not in possible_rows:
-        print(f"Possible rows: {possible_rows}")
-        row = input("Please enter the row you would like to target: ")
-        if row.upper() not in possible_rows:
-            print("Invalid selection, Please enter a valid row")
+        while row not in ROWS:
+            print(f"Possible rows: {ROWS}")
+            row = input("Please enter the row you would like to target: ")
+            if row not in ROWS:
+                print("Invalid selection, Please enter a valid row")
 
-    while column not in possible_columns:
-        print(f"Possible columns: {possible_columns}")
-        column = input("Please enter the column you would like to target: ")
-        if column not in possible_columns:
-            print("Invalid selection, Please enter a valid column")
+        while column not in COLUMNS:
+            print(f"Possible columns: {COLUMNS}")
+            column = input("Please enter the column you would like to target: ")
+            if column not in COLUMNS:
+                print("Invalid selection, Please enter a valid column")
 
-    target = [row.upper(), column]
-    return target
+        target = [row, column]
+        return target
 
-
-def switch_player(player: int) -> int:
-    """
-    The function switch which players turn it is
-    :param player: the current player turn
-    :return: the next player to have a turn
-    """
-    if player == 1:
-        player = 2
-    else:
-        player = 1
-    return player
-
+def run_game():
+    board_cpu, board_player = GameBoard(), GameBoard()
+    board_player.add_ships()
+    board_player.print_board()
 
 if __name__ == "__main__":
-    player_cpu = Player("cpu")
-    player_1 = Player("player1")
-    for ship in ships:
-        ship_to_add = Ship(ship)
-        player_1.add_ship(ship_to_add)
-
-    for ship in player_1.ships:
-        ship.add_positions()
-
-    for ship in ships:
-        ship_to_add = Ship(ship)
-        player_cpu.add_ship(ship_to_add)
-    for ship in player_cpu.ships:
-        ship.add_random_positions()
-
-for ship in player_cpu.ships:
-    print(ship.positions)
-
+   run_game()
+    
+ 
